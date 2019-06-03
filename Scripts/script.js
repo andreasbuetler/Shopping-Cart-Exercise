90d297d61bfaf9fc73fef2120133cf8b8eb3bf04
Anderas Bütler <andreas.buetler@zhdk.ch>
2019-06-03 22:48:42 +0200

    added CSS grid and js filling content

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,20 +1,38 @@
 $(document).ready(function() {
 
-var content[
+var content = [
   {
     title: 'Nummer1',
-    text: 'blabla',
-  }
+    text: 'blabla'
+  },
   {
     title: 'Nummer2',
-    text: 'blabla',
+    text: 'blabla'
+  },
+  {
+    title: 'Nummer2',
+    text: 'blabla'
+  },
+  {
+    title: 'Nummer3 halllo du was geht?',
+    text: 'blablasldjflasjdflöjasödlfsdlöfjlskdfjlökasdjflköajsdlfkjaslkdfjlkasjdfljsdfljlsdj'
   }
+
 ];
 
-
-
-
-
-
+function fillContent(content){
+for (var i = 0; i < content.length; i++){
+  var $populatedTemplate = $(populateTemplate(content[i]));
+  $('#rect').append($populatedTemplate);
+}
 
 }
+
+function populateTemplate(content){
+  var filler = `<div class="content-wrapper "><h1>${content.title}<h1/><p>${content.text}<p/><div/>`
+  console.log("content", $(filler));
+  return filler;
+}
+
+fillContent(content);
+});
