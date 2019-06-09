7b7e1766c49d885be800b3e4ae0ff37c04f0b20c
Anderas Bütler <andreas.buetler@zhdk.ch>
2019-06-09 13:30:39 +0200

    last commit

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,55 +4,54 @@ var content = [
   {
     title: 'Object 1',
     text: 'blabla',
-    id:"1",
+
   },
   {
     title: 'Object 2',
     text: 'blabla',
-    id:'2',
+
 
   },
   {
     title: 'Object 3',
     text: 'blabla',
-    id:'3',
+
 
   },
   {
     title: 'Object 4',
     text: 'blablasldjflasjdflöjasödlfsdlöfjlskdfjlökasdjflköajsdlfkjaslkdfjlkasjdfljsdfljlsdj',
-    id:4,
   },
   {
     title: 'Object 5',
     text: 'blabla',
-    id:5,
+
   },
   {
     title: 'Object 6',
     text: 'blabla',
-    id:6,
-  },
+},
   {
     title: 'Object 7',
     text: 'blabla',
-    
+
   }
 
 ];
 
-function fillContent(content){
+function fillContent(something){
 for (var i = 0; i < content.length; i++){
-  var $populatedTemplate = $(populateTemplate(content[i]));
+  var $populatedTemplate = $(populateTemplate(something[i]));
   $('#rect').append($populatedTemplate);
 }
 
 }
 
 function populateTemplate(content){
-  var filler = `<div class="content-wrapper id=${content.id}>
-  <input class="js-input" type="radio" name="fruit" value="${content.id}" id="${content.id}">
-  <h1>${content.title}<h1/><p>${content.text}<p/><div/>`;
+  var filler = `<div class="content-wrapper" >
+                <h1>${content.title}<h1/>
+                <span class="text">${content.text}<span/>
+                <div/>`;
   console.log("content", $(filler));
   return filler;
 }
@@ -70,6 +69,7 @@ $('.content-wrapper').click(function(event){
     var id = event.target.id;
     console.log('id = ' + id);
     $( this ).toggleClass( "open" );
+    $('text').toggleClass("open");
 
 })
 //
