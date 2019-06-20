a5e7fb03c3fc218a3d4ce55a360ed7d9ee176181
Anderas Bütler <andreas.buetler@zhdk.ch>
2019-06-20 12:49:14 +0200

    open toggle + styling

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,26 +2,35 @@ $(document).ready(function() {
 
 var content = [
   {
-    title: 'Object 1',
-    text: 'blabla',
+    title: 'AN-100',
+    manufacturer: 'Anchor Audio',
+    description: 'lorem ipsum',
+    img: 'AN_100.jpgs'
 
   },
   {
-    title: 'Object 2',
-    text: 'blabla',
+    title: 'Explorer PA-2500',
+    manufacturer: 'Anchor Audio',
+    description: 'Einzel Aktiv Lautsprecher mit Griff. Gut für kleine Vorträge oder Vorführungen. Dazugehöriges Stativ ist separat ausleihbar.',
+    img: 'explorer.jpg'
 
-
-  },
-  {
-    title: 'Object 3',
-    text: 'blabla',
 
 
   },
   {
-    title: 'Object 4',
-    text: 'blablasldjflasjdflöjasödlfsdlöfjlskdfjlökasdjflköajsdlfkjaslkdfjlkasjdfljsdfljlsdj',
+    title: 'DVX 8',
+    manufacturer: 'dB Technologies',
+    description: 'Die DVX D8 ist eine sehr leistungsfähige aktive 8“/1“ 2-Wege Lautsprecherbox, die speziell zur Wiedergabe von Sprache, Hintergrundmusik und Musikeinspielungen konzipiert wurde.',
+    img: 'dvx_d8.png'
+
+
   },
+  {
+    title: 'SRM 450 V2',
+    manufacturer: 'Mackie',
+    description: 'Bestückung: 12”/1” Abstrahlwinkel: 90°x45° 400 Watt (Biamped 100 Watt Hochtöner 300 Watt Mittel Tieftöner) Frequenzgang 55-18000Hz SPL max. 127db Flugfähig (M10 Eyebolt 180321) Low Cut Schaltbar (75Hz) Mic oder Line Input XLR female Anschluss Abme',
+    img: 'mackie_srm450_1.jpeg'
+    },
   {
     title: 'Object 5',
     text: 'blabla',
@@ -74,13 +83,15 @@ for (var i = 0; i < content.length; i++){
 }
 
 function populateTemplate(content){
-  var filler = `<div class="content-wrapper grid-item" >
+  var filler = `<div class="content-wrapper grid-item" id= >
                 <h1>${content.title}</h1>
+                <h2>${content.manufacturer}</h2>
+                <img src="./Data/Pictures/${content.img}" alt="" style='height: 50px; width: 100%; object-fit: contain'>
+                <div class="hidden" id="1">
+                <span  id="text">${content.description}<br></span>
+                <button class="button-secondary" type="button"> In den Warenkorb </button>
 
-                <span class="hidden" id="text">${content.text}<br></span>
-                <button type="button"> Test Button </button>
-                <img src="./Data/Pictures/${content.img}" alt="" style='height: 100%; width: 100%; object-fit: contain'>
-
+                <div/>
                 <div/>`;
   return filler;
 }
@@ -94,32 +105,38 @@ var contentWrapper = $('.content-wrapper');
 var input = $('input');
 
 
-// $('.content-wrapper').click(function(event){
-//     var id = event.target.id;
-//     console.log('id = ' + id);
-//     $( this ).toggleClass( "open" );
-//     $(this).show();
-//
-//   });
+$('.content-wrapper').click(function(event){
+    // var target = $( event.target );
+    // target.toggleClass(".hidden");
+    // $(".hidden").toggle();
+    var parent_box;
+    parent_box = $(this).closest('.content-wrapper');
+    parent_box.find('#1').toggle('hidden');
+    // parent_box.toggle('.hidden');
+
+  });
+r
+
+      $('.testgrid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    horizontalOrder: true,
+    columnwidth: '.grid-item',
+
+  });
 
 var $testgrid = $('.testgrid').masonry({
-  columnWidth: 80
+  // columnWidth: 80
 });
 // change size of item by toggling gigante class
 $testgrid.on( 'click', '.grid-item', function() {
-  $(this).toggleClass('open');
+  // $(this).toggleClass('.hidden');
   // trigger layout after item size changes
-  $testgrid.masonry('layout');
+   // $testgrid.masonry('layout');
+
 });
 
 
-    $('.testgrid').masonry({
-  // options
-  itemSelector: '.grid-item',
-  horizontalOrder: true,
-  columnwidth: '.grid-item',
-  percentPosition: true,
-});
 
 
 
